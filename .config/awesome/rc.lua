114c71e6df85d2bde926e167cfa8484cad0cf7ad
jackson <9527380+Jaksuhn@users.noreply.github.com>
2022-01-09 11:34:42 -0500

    added touchpad autostart

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -43,6 +43,9 @@ screen.connect_signal("request::desktop_decoration", function(s)
     awful.tag({"1", "2", "3", "4", "5"}, s, awful.layout.layouts[1])
 end)
 
+-- Autostart
+require("autostart")
+
 -- Import Daemons and Widgets
 require("signal")
 require("ui")
