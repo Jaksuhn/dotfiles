18a69759de64eecef57fced7fdd5a54bf272c9a9
jackson <9527380+Jaksuhn@users.noreply.github.com>
2022-02-22 21:29:23 -0500

    removed install_on function

--- install.py
+++ install.py
@@ -115,204 +115,6 @@ while github_access_token := input("Github Access Token (default: none): "):
         continue
     break
 
-
-def install_on(mountpoint):
-    with archinstall.Installer(mountpoint) as i:
-        # Strap in the base system, add a boot loader and configure
-        # some other minor details as specified by this profile and user.
-        mirror_regions = {DOWNLOAD_REGION: archinstall.list_mirrors().get(DOWNLOAD_REGION)}
-        archinstall.use_mirrors(mirror_regions)
-
-        i.minimal_installation()
-
-        i.set_hostname(hostname)
-        i.set_mirrors(mirror_regions)
-        i.add_bootloader()
-
-        # enable networking
-        i.copy_iso_network_config(enable_services=True)
-
-        i.arch_chroot(r"sed -i '/\[multilib\]/,/Include/''s/^#//' /etc/pacman.conf")  # enable 32-bit apps
-        i.arch_chroot(r"sed -i 's/#\(Color\)/\1/' /etc/pacman.conf")  # enable coloured output
-        i.arch_chroot(r"sed -i '/VerbosePkgLists/s/^#//' /etc/pacman.conf")  # enable detailed output
-        i.arch_chroot(
-            r"sed -i 's/# Parallel Downloads = [0-9]/Parallel Downloads = 10/g' /etc/pacman.conf"
-        )  # parallel downloads
-
-        # fix potential gpg key issues when using an older ISO
-        i.log(i.arch_chroot("pacman -Sy && pacman -S archlinux-keyring --noconfirm"), level=logging.INFO)
-        i.log(i.add_additional_packages(dependencies), level=logging.INFO)
-
-        # create root account, grant root access
-        i.user_set_pw("root", str(root_password))
-        i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL) ALL\)/\1/' /etc/sudoers")
-
-        # create user, change login shell
-        i.user_create(str(user), str(user_password))
-        i.log(i.arch_chroot(f'chsh -s /usr/bin/zsh "{user}"'), level=logging.INFO)
-        # i.log(i.arch_chroot(f"usermod -aG wheel {user}"), level=logging.INFO)
-        i.log(i.arch_chroot(f"sed -i '/root ALL=(ALL:ALL) ALL/a{user} ALL=(ALL:ALL) ALL' /etc/sudoers"))
-
-        # the profiles are tricky to customise from chroot. May remove and place in a post-install .sh file
-        if profile == "bspwm":
-            i.install_profile("xorg")
-            i.add_additional_packages(bspwm_packages)
-            i.enable_service("lightdm")
-            i.arch_chroot(
-                f"su {user} -c 'mkdir ~/temp_configs && cd ~/temp_configs && git clone https://github.com/joni22u/dotfiles . && cp -rb . ~/.config && rm -rf ~/temp_configs'"
-            )
-        elif profile == "kde":
-            i.install_profile(profile)
-            i.arch_chroot("lookandfeeltool -a GruvboxPlasma")
-        elif profile == "gnome":
-            i.install_profile(profile)
-            dependencies_aur.append("gnome-shell-extension-material-shell")
-            # remove 'bloat'
-            # i.log(
-            #     i.arch_chroot(
-            #         "pacman -Rns gnome-software gnome-weather gnome-contacts gnome-calendar gnome-boxes epiphany gnome-books gedit gnome-music simple-scan gnome-maps gnome-photos totem gnome-clocks gnome-calculator eog sushi evince file-roller gnome-screenshot gnome-characters gnome-backgrounds"
-            #     )
-            # )
-        elif profile == "awesome":
-            # https://github.com/JavaCafe01/dotfiles/ (mostly)
-            # https://gitlab.com/ihciM/dotfiles
-            i.install_profile("xorg")
-            i.add_additional_packages(awesome_packages)
-            dependencies_aur.append("awesome-git")
-            i.enable_service("lightdm")
-            # set awesome to run over xfce
-            i.arch_chroot(
-                "xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd"
-            )
-            i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa awesome")
-            i.arch_chroot('xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"')
-            i.arch_chroot('xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrainsMono Nerd Font 10"')
-            # look into https://github.com/vlfldr/rofi-wifi-menu or https://github.com/ericmurphyxyz/rofi-wifi-menu
-        elif profile == "xfce4":
-            i.install_profile(profile)
-            i.add_additional_packages(xfce4_packages)
-            dependencies_aur.append("lightdm-webkit2-theme-glorious")
-            i.log(
-                i.arch_chroot(
-                    f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-theme . && $SHELL install.sh'"
-                ),
-                level=logging.INFO,
-            )
-            i.log(
-                i.arch_chroot(
-                    f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-icon-theme . && $SHELL install.sh && cd src/cursors && $SHELL install.sh'"
-                ),
-                level=logging.INFO,
-            )
-            # Set default lightdm greeter to lightdm-webkit2-greeter
-            i.log(
-                i.arch_chroot(
-                    "sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf"
-                ),
-                level=logging.INFO,
-            )
-            # Set default lightdm-webkit2-greeter theme to Glorious
-            i.log(
-                i.arch_chroot(
-                    "sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
-                ),
-                level=logging.INFO,
-            )
-            i.log(
-                i.arch_chroot(
-                    "sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
-                ),
-                level=logging.INFO,
-            )
-        else:
-            i.install_profile(profile)
-
-        # enable systemd services
-        i.log(
-            i.enable_service("iwd", "NetworkManager", "systemd-timesyncd", "docker", "bluetooth", "fstrim.timer"),
-            level=logging.INFO,
-        )
-        i.arch_chroot(r"sed -i 's/[#]*\(AutoEnable=\)\(true\|false\)/\1true/' /etc/bluetooth/main.conf")
-
-        i.arch_chroot(f'su {user} -c \'ssh-keygen -t ed25519 -C "{user}@{hostname}" -f ~/.ssh/id_ed25519 -N ""\'')
-        i.arch_chroot(f"su {user} -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'")
-
-        if github_access_token:
-            with open(f"{i.target}/home/{user}/.ssh/id_ed25519.pub", "r") as key:
-                requests.post(
-                    url="https://api.github.com/user/keys",
-                    json={"title": f"{user}@{hostname}", "key": f"{key.read().strip()}"},
-                    headers={"Authorization": f"token {github_access_token}"},
-                )
-
-        # clone dotfiles
-        i.log(
-            i.arch_chroot(
-                f"su {user} -c 'cd $(mktemp -d) && git clone -b {BRANCH} {'git@github.com:jaksuhn/dotfiles.git' if github_access_token else 'https://github.com/jaksuhn/dotfiles.git'} . && cp -rb . ~'"
-            ),
-            level=logging.INFO,
-        )
-        i.log(i.arch_chroot(f"su {user} -c 'rm -rf ~/.git'"), level=logging.INFO)
-        i.log(i.arch_chroot(f"su {user} -c 'chmod +x ~/.config/startup/touchpad.sh'"), level=logging.INFO)
-
-        # add more processors to the makepkg build system
-        i.arch_chroot(r"sed -i 's/#\(MAKEFLAGS=\).*/\1\"-j$(($(nproc)-2))\"/' /etc/makepkg.conf")
-
-        # install paru and aur packages
-        i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers")
-        i.log(
-            i.arch_chroot(
-                f"su {user} -c 'cd $(mktemp -d) && git clone https://aur.archlinux.org/paru-bin.git . && makepkg -sim --noconfirm'"
-            ),
-            level=logging.INFO,
-        )
-        i.log(
-            i.arch_chroot(f'su {user} -c "paru -Sy --nosudoloop --needed --noconfirm {" ".join(dependencies_aur)}"'),
-            level=logging.INFO,
-        )
-        i.arch_chroot(r"sed -i 's/\(%wheel ALL=(ALL) NOPASSWD: ALL\)/# \1/' /etc/sudoers")
-        i.log(i.arch_chroot(f"chown -R {user}:{user} /home/{user}/paru"), level=logging.INFO)
-
-        # setup yadm
-        i.log(
-            i.arch_chroot(f"su {user} -c 'yadm clone --branch {BRANCH} https://github.com/jaksuhn/dotfiles'"),
-            level=logging.INFO,
-        )
-        i.arch_chroot(f"su {user} -c 'git config --global user.name \"{user}\"'")
-        i.arch_chroot(f"su {user} -c 'git config --global user.email \"{user}@{hostname}\"'")
-
-        # fetch nnn plugins
-        i.log(
-            i.arch_chroot(
-                f"su {user} -c 'curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh'"
-            ),
-            level=logging.INFO,
-        )
-
-        # adding material icons
-        i.log("\ninstalling material icons")
-        i.log(i.arch_chroot("mkdir -p /usr/local/share/fonts/"))
-        i.log(
-            i.arch_chroot(
-                "wget -O /usr/local/share/fonts/material-design-icons.ttf https://github.com/Templarian/MaterialDesign-Font/raw/master/MaterialDesignIconsDesktop.ttf"
-            )
-        )
-
-        # install cht.sh
-        i.log("\ninstalling cht.sh")
-        r = requests.get("https://cht.sh/:cht.sh")
-        with open(f"{i.target}/usr/local/bin/cht.sh", "wb") as cht:
-            cht.write(r.content)
-        i.log(i.arch_chroot(f"setfacl -m u:{user}:rx /usr/local/bin/cht.sh"), level=logging.INFO)
-        i.log(i.arch_chroot(f"su {user} -c 'curl https://cheat.sh/:zsh > ~/.config/zsh/_cht'"), level=logging.INFO)
-
-        if input("Go into shell (y/n)?") == "y":
-            try:
-                i.drop_to_shell()
-            except:
-                pass
-
-
 if archinstall.arguments["harddrive"]:
     archinstall.arguments["harddrive"].keep_partitions = False
 
@@ -347,4 +149,195 @@ if archinstall.arguments["harddrive"]:
 
         boot.mount("/mnt/boot")
 
-install_on("/mnt")
+with archinstall.Installer("/mnt") as i:
+    # Strap in the base system, add a boot loader and configure
+    # some other minor details as specified by this profile and user.
+    mirror_regions = {DOWNLOAD_REGION: archinstall.list_mirrors().get(DOWNLOAD_REGION)}
+    archinstall.use_mirrors(mirror_regions)
+
+    i.minimal_installation()
+
+    i.set_hostname(hostname)
+    i.set_mirrors(mirror_regions)
+    i.add_bootloader()
+
+    # enable networking
+    i.copy_iso_network_config(enable_services=True)
+
+    i.arch_chroot(r"sed -i '/\[multilib\]/,/Include/''s/^#//' /etc/pacman.conf")  # enable 32-bit apps
+    i.arch_chroot(r"sed -i 's/#\(Color\)/\1/' /etc/pacman.conf")  # enable coloured output
+    i.arch_chroot(r"sed -i '/VerbosePkgLists/s/^#//' /etc/pacman.conf")  # enable detailed output
+    i.arch_chroot(
+        r"sed -i 's/# Parallel Downloads = [0-9]/Parallel Downloads = 10/g' /etc/pacman.conf"
+    )  # parallel downloads
+
+    # fix potential gpg key issues when using an older ISO
+    i.log(i.arch_chroot("pacman -Sy && pacman -S archlinux-keyring --noconfirm"), level=logging.INFO)
+    i.log(i.add_additional_packages(dependencies), level=logging.INFO)
+
+    # create root account, grant root access
+    i.user_set_pw("root", str(root_password))
+    i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL) ALL\)/\1/' /etc/sudoers")
+
+    # create user, change login shell
+    i.user_create(str(user), str(user_password))
+    i.log(i.arch_chroot(f'chsh -s /usr/bin/zsh "{user}"'), level=logging.INFO)
+    # i.log(i.arch_chroot(f"usermod -aG wheel {user}"), level=logging.INFO)
+    i.log(i.arch_chroot(f"sed -i '/root ALL=(ALL:ALL) ALL/a{user} ALL=(ALL:ALL) ALL' /etc/sudoers"))
+
+    # the profiles are tricky to customise from chroot. May remove and place in a post-install .sh file
+    if profile == "bspwm":
+        i.install_profile("xorg")
+        i.add_additional_packages(bspwm_packages)
+        i.enable_service("lightdm")
+        i.arch_chroot(
+            f"su {user} -c 'mkdir ~/temp_configs && cd ~/temp_configs && git clone https://github.com/joni22u/dotfiles . && cp -rb . ~/.config && rm -rf ~/temp_configs'"
+        )
+    elif profile == "kde":
+        i.install_profile(profile)
+        i.arch_chroot("lookandfeeltool -a GruvboxPlasma")
+    elif profile == "gnome":
+        i.install_profile(profile)
+        dependencies_aur.append("gnome-shell-extension-material-shell")
+        # remove 'bloat'
+        # i.log(
+        #     i.arch_chroot(
+        #         "pacman -Rns gnome-software gnome-weather gnome-contacts gnome-calendar gnome-boxes epiphany gnome-books gedit gnome-music simple-scan gnome-maps gnome-photos totem gnome-clocks gnome-calculator eog sushi evince file-roller gnome-screenshot gnome-characters gnome-backgrounds"
+        #     )
+        # )
+    elif profile == "awesome":
+        # https://github.com/JavaCafe01/dotfiles/ (mostly)
+        # https://gitlab.com/ihciM/dotfiles
+        i.install_profile("xorg")
+        i.add_additional_packages(awesome_packages)
+        dependencies_aur.append("awesome-git")
+        i.enable_service("lightdm")
+        # set awesome to run over xfce
+        i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd")
+        i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa awesome")
+        i.arch_chroot('xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"')
+        i.arch_chroot('xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrainsMono Nerd Font 10"')
+        # look into https://github.com/vlfldr/rofi-wifi-menu or https://github.com/ericmurphyxyz/rofi-wifi-menu
+    elif profile == "xfce4":
+        i.install_profile(profile)
+        i.add_additional_packages(xfce4_packages)
+        dependencies_aur.append("lightdm-webkit2-theme-glorious")
+        i.log(
+            i.arch_chroot(
+                f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-theme . && $SHELL install.sh'"
+            ),
+            level=logging.INFO,
+        )
+        i.log(
+            i.arch_chroot(
+                f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-icon-theme . && $SHELL install.sh && cd src/cursors && $SHELL install.sh'"
+            ),
+            level=logging.INFO,
+        )
+        # Set default lightdm greeter to lightdm-webkit2-greeter
+        i.log(
+            i.arch_chroot(
+                "sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf"
+            ),
+            level=logging.INFO,
+        )
+        # Set default lightdm-webkit2-greeter theme to Glorious
+        i.log(
+            i.arch_chroot(
+                "sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
+            ),
+            level=logging.INFO,
+        )
+        i.log(
+            i.arch_chroot(
+                "sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
+            ),
+            level=logging.INFO,
+        )
+    else:
+        i.install_profile(profile)
+
+    # enable systemd services
+    i.log(
+        i.enable_service("iwd", "NetworkManager", "systemd-timesyncd", "docker", "bluetooth", "fstrim.timer"),
+        level=logging.INFO,
+    )
+    i.arch_chroot(r"sed -i 's/[#]*\(AutoEnable=\)\(true\|false\)/\1true/' /etc/bluetooth/main.conf")
+
+    i.arch_chroot(f'su {user} -c \'ssh-keygen -t ed25519 -C "{user}@{hostname}" -f ~/.ssh/id_ed25519 -N ""\'')
+    i.arch_chroot(f"su {user} -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'")
+
+    if github_access_token:
+        with open(f"{i.target}/home/{user}/.ssh/id_ed25519.pub", "r") as key:
+            requests.post(
+                url="https://api.github.com/user/keys",
+                json={"title": f"{user}@{hostname}", "key": f"{key.read().strip()}"},
+                headers={"Authorization": f"token {github_access_token}"},
+            )
+
+    # clone dotfiles
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone -b {BRANCH} {'git@github.com:jaksuhn/dotfiles.git' if github_access_token else 'https://github.com/jaksuhn/dotfiles.git'} . && cp -rb . ~'"
+        ),
+        level=logging.INFO,
+    )
+    i.log(i.arch_chroot(f"su {user} -c 'rm -rf ~/.git'"), level=logging.INFO)
+    i.log(i.arch_chroot(f"su {user} -c 'chmod +x ~/.config/startup/touchpad.sh'"), level=logging.INFO)
+
+    # add more processors to the makepkg build system
+    i.arch_chroot(r"sed -i 's/#\(MAKEFLAGS=\).*/\1\"-j$(($(nproc)-2))\"/' /etc/makepkg.conf")
+
+    # install paru and aur packages
+    i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers")
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone https://aur.archlinux.org/paru-bin.git . && makepkg -sim --noconfirm'"
+        ),
+        level=logging.INFO,
+    )
+    i.log(
+        i.arch_chroot(f'su {user} -c "paru -Sy --nosudoloop --needed --noconfirm {" ".join(dependencies_aur)}"'),
+        level=logging.INFO,
+    )
+    i.arch_chroot(r"sed -i 's/\(%wheel ALL=(ALL) NOPASSWD: ALL\)/# \1/' /etc/sudoers")
+    i.log(i.arch_chroot(f"chown -R {user}:{user} /home/{user}/paru"), level=logging.INFO)
+
+    # setup yadm
+    i.log(
+        i.arch_chroot(f"su {user} -c 'yadm clone --branch {BRANCH} https://github.com/jaksuhn/dotfiles'"),
+        level=logging.INFO,
+    )
+    i.arch_chroot(f"su {user} -c 'git config --global user.name \"{user}\"'")
+    i.arch_chroot(f"su {user} -c 'git config --global user.email \"{user}@{hostname}\"'")
+
+    # fetch nnn plugins
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh'"
+        ),
+        level=logging.INFO,
+    )
+
+    # adding material icons
+    i.log("\ninstalling material icons")
+    i.log(i.arch_chroot("mkdir -p /usr/local/share/fonts/"))
+    i.log(
+        i.arch_chroot(
+            "wget -O /usr/local/share/fonts/material-design-icons.ttf https://github.com/Templarian/MaterialDesign-Font/raw/master/MaterialDesignIconsDesktop.ttf"
+        )
+    )
+
+    # install cht.sh
+    i.log("\ninstalling cht.sh")
+    r = requests.get("https://cht.sh/:cht.sh")
+    with open(f"{i.target}/usr/local/bin/cht.sh", "wb") as cht:
+        cht.write(r.content)
+    i.log(i.arch_chroot(f"setfacl -m u:{user}:rx /usr/local/bin/cht.sh"), level=logging.INFO)
+    i.log(i.arch_chroot(f"su {user} -c 'curl https://cheat.sh/:zsh > ~/.config/zsh/_cht'"), level=logging.INFO)
+
+    if input("Go into shell (y/n)?") == "y":
+        try:
+            i.drop_to_shell()
+        except:
+            pass
