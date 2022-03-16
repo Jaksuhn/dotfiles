824b4414f6f088a4385778773cbba56850129609
jackson <9527380+Jaksuhn@users.noreply.github.com>
2022-03-16 12:01:00 -0400

    refactored

--- install.py
+++ install.py
@@ -119,6 +119,99 @@ while github_access_token := input("Github Access Token (default: none): "):
     break
 
 
+def setup_awesome(i):
+    # https://github.com/JavaCafe01/dotfiles/ (mostly)
+    # https://gitlab.com/ihciM/dotfiles
+    i.install_profile("xorg")
+    i.add_additional_packages(awesome_packages)
+    dependencies_aur.append("awesome-git")
+    i.enable_service("lightdm")
+    # set awesome to run over xfce
+    i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd")
+    i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa awesome")
+    i.arch_chroot('xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"')
+    i.arch_chroot('xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrainsMono Nerd Font 10"')
+    # look into https://github.com/vlfldr/rofi-wifi-menu or https://github.com/ericmurphyxyz/rofi-wifi-menu
+
+
+def setup_xfce(i):
+    i.install_profile(profile)
+    i.add_additional_packages(xfce4_packages)
+    dependencies_aur.append("lightdm-webkit2-theme-glorious")
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-theme . && $SHELL install.sh'"
+        ),
+        level=logging.INFO,
+    )
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-icon-theme . && $SHELL install.sh && cd src/cursors && $SHELL install.sh'"
+        ),
+        level=logging.INFO,
+    )
+    # Set default lightdm greeter to lightdm-webkit2-greeter
+    # Cannot get sed -i 's/^#\(greeter-session=\).*/\1lightdm-webkit2-greeter/'
+    # to work. I'll change this if I find a fix.
+    try:
+        with open("/etc/lightdm/lightdm.conf") as r:
+            data = re.sub(r"(^#greeter-session=.*)", "greeter-session=lightdm-webkit2-greeter", r.read())
+        with open("/etc/lightdm/lightdm.conf", "w") as w:
+            w.write(data)
+    except:
+        pass
+    # Set default lightdm-webkit2-greeter theme to Glorious
+    i.log(
+        i.arch_chroot(
+            "sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
+        ),
+        level=logging.INFO,
+    )
+    i.log(
+        i.arch_chroot(
+            "sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
+        ),
+        level=logging.INFO,
+    )
+
+
+def clone_dots(i):
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone -b {BRANCH} {'git@github.com:jaksuhn/dotfiles.git' if github_access_token else 'https://github.com/jaksuhn/dotfiles.git'} . && cp -rb . ~'"
+        ),
+        level=logging.INFO,
+    )
+    i.log(i.arch_chroot(f"su {user} -c 'rm -rf ~/.git'"), level=logging.INFO)
+    i.log(i.arch_chroot(f"su {user} -c 'chmod +x ~/.config/startup/touchpad.sh'"), level=logging.INFO)
+
+
+def setup_paru(i):
+    # install paru and aur packages
+    i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers")
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'cd $(mktemp -d) && git clone https://aur.archlinux.org/paru-bin.git . && makepkg -sim --noconfirm'"
+        ),
+        level=logging.INFO,
+    )
+    i.log(
+        i.arch_chroot(f'su {user} -c "paru -Sy --nosudoloop --needed --noconfirm {" ".join(dependencies_aur)}"'),
+        level=logging.INFO,
+    )
+    # i.arch_chroot(r"sed -i 's/\(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/# \1/' /etc/sudoers")
+    i.log(i.arch_chroot(f"chown -R {user}:{user} /home/{user}/paru"), level=logging.INFO)
+
+
+def setup_yadm(i):
+    i.log(
+        i.arch_chroot(f"su {user} -c 'yadm clone --branch {BRANCH} https://github.com/jaksuhn/dotfiles'"),
+        level=logging.INFO,
+    )
+    i.arch_chroot(f"su {user} -c 'git config --global user.name \"{user}\"'")
+    i.arch_chroot(f"su {user} -c 'git config --global user.email \"{user}@{hostname}\"'")
+
+
 def write_hosts(i):
     data = """\
         127.0.0.1   localhost
@@ -131,6 +224,34 @@ def write_hosts(i):
     """
     with open("/etc/hosts", "w") as w:
         w.write(cleandoc(data))
+
+
+def get_nnn_plugs(i):
+    i.log(
+        i.arch_chroot(
+            f"su {user} -c 'curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh'"
+        ),
+        level=logging.INFO,
+    )
+
+
+def get_material_icons(i):
+    i.log("\ninstalling material icons")
+    i.log(i.arch_chroot("mkdir -p /usr/local/share/fonts/"))
+    i.log(
+        i.arch_chroot(
+            "wget -O /usr/local/share/fonts/material-design-icons.ttf https://github.com/Templarian/MaterialDesign-Font/raw/master/MaterialDesignIconsDesktop.ttf"
+        )
+    )
+
+
+def setup_cht(i):
+    i.log("\ninstalling cht.sh")
+    r = requests.get("https://cht.sh/:cht.sh")
+    with open(f"{i.target}/usr/local/bin/cht.sh", "wb") as cht:
+        cht.write(r.content)
+    i.log(i.arch_chroot(f"setfacl -m u:{user}:rx /usr/local/bin/cht.sh"), level=logging.INFO)
+    i.log(i.arch_chroot(f"su {user} -c 'curl https://cheat.sh/:zsh > ~/.config/zsh/_cht'"), level=logging.INFO)
 
 
 if archinstall.arguments["harddrive"]:
@@ -202,7 +323,6 @@ with archinstall.Installer("/mnt") as i:
     i.log(i.arch_chroot(f'chsh -s /usr/bin/zsh "{user}"'), level=logging.INFO)
     i.log(i.arch_chroot(f"sed -i '/root ALL=(ALL:ALL) ALL/a{user} ALL=(ALL:ALL) ALL' /etc/sudoers"))
 
-    # the profiles are tricky to customise from chroot. May remove and place in a post-install .sh file
     if profile == "bspwm":
         i.install_profile("xorg")
         i.add_additional_packages(bspwm_packages)
@@ -223,57 +343,9 @@ with archinstall.Installer("/mnt") as i:
         #     )
         # )
     elif profile == "awesome":
-        # https://github.com/JavaCafe01/dotfiles/ (mostly)
-        # https://gitlab.com/ihciM/dotfiles
-        i.install_profile("xorg")
-        i.add_additional_packages(awesome_packages)
-        dependencies_aur.append("awesome-git")
-        i.enable_service("lightdm")
-        # set awesome to run over xfce
-        i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd")
-        i.arch_chroot("xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa awesome")
-        i.arch_chroot('xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto 10"')
-        i.arch_chroot('xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "JetBrainsMono Nerd Font 10"')
-        # look into https://github.com/vlfldr/rofi-wifi-menu or https://github.com/ericmurphyxyz/rofi-wifi-menu
+        setup_awesome(i)
     elif profile == "xfce4":
-        i.install_profile(profile)
-        i.add_additional_packages(xfce4_packages)
-        dependencies_aur.append("lightdm-webkit2-theme-glorious")
-        i.log(
-            i.arch_chroot(
-                f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-theme . && $SHELL install.sh'"
-            ),
-            level=logging.INFO,
-        )
-        i.log(
-            i.arch_chroot(
-                f"su {user} -c 'cd $(mktemp -d) && git clone https://github.com/vinceliuice/Qogir-icon-theme . && $SHELL install.sh && cd src/cursors && $SHELL install.sh'"
-            ),
-            level=logging.INFO,
-        )
-        # Set default lightdm greeter to lightdm-webkit2-greeter
-        # Cannot get sed -i 's/^#\(greeter-session=\).*/\1lightdm-webkit2-greeter/'
-        # to work. I'll change this if I find a fix.
-        try:
-            with open("/etc/lightdm/lightdm.conf") as r:
-                data = re.sub(r"(^#greeter-session=.*)", "greeter-session=lightdm-webkit2-greeter", r.read())
-            with open("/etc/lightdm/lightdm.conf", "w") as w:
-                w.write(data)
-        except:
-            pass
-        # Set default lightdm-webkit2-greeter theme to Glorious
-        i.log(
-            i.arch_chroot(
-                "sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
-            ),
-            level=logging.INFO,
-        )
-        i.log(
-            i.arch_chroot(
-                "sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf"
-            ),
-            level=logging.INFO,
-        )
+        setup_xfce(i)
     else:
         i.install_profile(profile)
 
@@ -295,41 +367,13 @@ with archinstall.Installer("/mnt") as i:
                 headers={"Authorization": f"token {github_access_token}"},
             )
 
-    # clone dotfiles
-    i.log(
-        i.arch_chroot(
-            f"su {user} -c 'cd $(mktemp -d) && git clone -b {BRANCH} {'git@github.com:jaksuhn/dotfiles.git' if github_access_token else 'https://github.com/jaksuhn/dotfiles.git'} . && cp -rb . ~'"
-        ),
-        level=logging.INFO,
-    )
-    i.log(i.arch_chroot(f"su {user} -c 'rm -rf ~/.git'"), level=logging.INFO)
-    i.log(i.arch_chroot(f"su {user} -c 'chmod +x ~/.config/startup/touchpad.sh'"), level=logging.INFO)
+    clone_dots(i)
 
     # add more processors to the makepkg build system
     i.arch_chroot(r"sed -i 's/#\(MAKEFLAGS=\).*/\1\"-j$(($(nproc)-2))\"/' /etc/makepkg.conf")
 
-    # install paru and aur packages
-    i.arch_chroot(r"sed -i 's/# \(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers")
-    i.log(
-        i.arch_chroot(
-            f"su {user} -c 'cd $(mktemp -d) && git clone https://aur.archlinux.org/paru-bin.git . && makepkg -sim --noconfirm'"
-        ),
-        level=logging.INFO,
-    )
-    i.log(
-        i.arch_chroot(f'su {user} -c "paru -Sy --nosudoloop --needed --noconfirm {" ".join(dependencies_aur)}"'),
-        level=logging.INFO,
-    )
-    # i.arch_chroot(r"sed -i 's/\(%wheel ALL=(ALL:ALL) NOPASSWD: ALL\)/# \1/' /etc/sudoers")
-    i.log(i.arch_chroot(f"chown -R {user}:{user} /home/{user}/paru"), level=logging.INFO)
-
-    # setup yadm
-    i.log(
-        i.arch_chroot(f"su {user} -c 'yadm clone --branch {BRANCH} https://github.com/jaksuhn/dotfiles'"),
-        level=logging.INFO,
-    )
-    i.arch_chroot(f"su {user} -c 'git config --global user.name \"{user}\"'")
-    i.arch_chroot(f"su {user} -c 'git config --global user.email \"{user}@{hostname}\"'")
+    setup_paru(i)
+    setup_yadm(i)
 
     # write hosts file. For some reason, (recently) this is needed for firefox
     write_hosts(i)
@@ -337,30 +381,9 @@ with archinstall.Installer("/mnt") as i:
     # add screenshot folder for flameshot
     i.arch_chroot("mkdir -p ~/Screenshots")
 
-    # fetch nnn plugins
-    i.log(
-        i.arch_chroot(
-            f"su {user} -c 'curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh'"
-        ),
-        level=logging.INFO,
-    )
-
-    # adding material icons
-    i.log("\ninstalling material icons")
-    i.log(i.arch_chroot("mkdir -p /usr/local/share/fonts/"))
-    i.log(
-        i.arch_chroot(
-            "wget -O /usr/local/share/fonts/material-design-icons.ttf https://github.com/Templarian/MaterialDesign-Font/raw/master/MaterialDesignIconsDesktop.ttf"
-        )
-    )
-
-    # install cht.sh
-    i.log("\ninstalling cht.sh")
-    r = requests.get("https://cht.sh/:cht.sh")
-    with open(f"{i.target}/usr/local/bin/cht.sh", "wb") as cht:
-        cht.write(r.content)
-    i.log(i.arch_chroot(f"setfacl -m u:{user}:rx /usr/local/bin/cht.sh"), level=logging.INFO)
-    i.log(i.arch_chroot(f"su {user} -c 'curl https://cheat.sh/:zsh > ~/.config/zsh/_cht'"), level=logging.INFO)
+    get_nnn_plugs(i)
+    get_material_icons(i)
+    setup_cht(i)
 
     if input("Go into shell (y/n)?") == "y":
         try:
